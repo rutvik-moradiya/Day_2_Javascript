e1ed5c44094d247d0f2b5100105903bb4a8d7250
rutvik.moradiya <rutvik.moradiya@estatic-infotech.com>
2022-01-19 10:31:42 +0530

    commit on 'master'

--- task_1/js/scripts.js
+++ task_1/js/scripts.js
@@ -6,91 +6,20 @@ function onTap() {
     var value = select.options[select.selectedIndex].value;
     console.log(value);
     switch (value) {
-        case "addition": //for addition
-            addtion();
+        case "RED": //for RED
+            alert("YOU HAVE CHOOSEN COLOR : RED");
             break;
-        case "subtraction": //for subtraction
-            subtraction();
-            break;
-
-        case "division": //for division
-            division();
+        case "GREEN": //for GREEN
+            alert("YOU HAVE CHOOSEN COLOR : GREEN");
             break;
 
-        case "multiplication": //for multiplication
-            multiplication();
-            break;
-        case "Modulus": //for Modulus
-            modulus();
-            break;
-        case "increment": //for increment
-            increment();
+        case "BLUE": //for BLUE
+            alert("YOU HAVE CHOOSEN COLOR : BLUE");
             break;
 
-        case "decrement": //for decrement
-            decrement();
+        case "OTHER": //for OTHER
+            alert("YOU HAVE CHOOSEN COLOR : OTHER");
             break;
 
     }
-
-}
-//for addition
-function addtion() {
-    a = 10;
-    b = 5;
-    result = a + b;
-    alert("Addition of Number " + a + " and " + b + " is :" + result);
-    // document.write(result);
-    console.log(result);
-}
-//for subtraction
-function subtraction(a, b) {
-    a = 10;
-    b = 5;
-    result = a - b;
-    alert("subtraction of Number " + a + " and " + b + " is :" + result);
-    console.log(result);
-}
-//for division
-function division(a, b) {
-    a = 10;
-    b = 5;
-    result = a / b;
-    alert("division of Number " + a + " and " + b + " is :" + result);
-    console.log(result);
-}
-//for multiplication
-function multiplication(a, b) {
-    a = 10;
-    b = 5;
-    result = a * b;
-    alert("multiplication of Number " + a + " and " + b + " is :" + result);
-    console.log(result);
-}
-//for modulus
-function modulus() {
-    a = 10;
-    b = 5;
-    result = a % b;
-    alert("modulus of Number " + result);
-    // document.write(result);
-    // console.log(result);
-}
-//for increment
-function increment(a, b) {
-    a = 10;
-    b = 5;
-    result = ++a;
-    result1 = ++b;
-    alert("increment of first Number  :" + result + " and " + "increment of second Number :" + result1);
-    console.log(result);
-}
-//for decrement
-function decrement(a, b) {
-    a = 10;
-    b = 5;
-    result = --a;
-    result1 = --b;
-    alert("decrement of first Number :" + result + " and " + "decrement of second Number :" + result1);
-    console.log(result);
 }
